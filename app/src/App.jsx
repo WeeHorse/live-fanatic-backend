3a95215474006d4894e87ab569da8411f3ab0ed7
Dannebanne94 <92047500+Dannebanne94@users.noreply.github.com>
2022-09-06 15:47:50 +0200

    Add and update stylesheets
    Comon settings for the application with fonts and colors

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
     return <>
         <h1>App</h1>
-        <p>Hello, World!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere in dolore neque, odit consequatur sit explicabo, nisi perspiciatis mollitia, obcaecati at labore doloremque nihil error et blanditiis. Consequuntur deserunt deleniti reiciendis eaque tempore odit, repudiandae quas a accusantium, ad ab dignissimos ullam. Doloremque at voluptas excepturi esse repellendus quidem ipsum.</p>
     </>
 }
 
