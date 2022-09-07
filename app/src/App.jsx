17f6da96224a3f609490a0ee9fe066ebbc77a953
krubear <torkild.kruber@gmail.com>
2022-09-07 16:04:35 +0200

    first commit hamburger menu

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,8 @@
+import Nav from './components/Hamburger/Nav'
 
 function App() {
     return <>
+        <Nav />
         <h1>App</h1>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere in dolore neque, odit consequatur sit explicabo, nisi perspiciatis mollitia, obcaecati at labore doloremque nihil error et blanditiis. Consequuntur deserunt deleniti reiciendis eaque tempore odit, repudiandae quas a accusantium, ad ab dignissimos ullam. Doloremque at voluptas excepturi esse repellendus quidem ipsum.</p>
     </>
