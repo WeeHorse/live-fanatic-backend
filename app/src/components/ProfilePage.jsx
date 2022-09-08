80ff16406b5eb4c15052d7f4a2d093cda81f7cb4
Dannebanne94 <92047500+Dannebanne94@users.noreply.github.com>
2022-09-08 12:03:41 +0200

    NJ-108-Add edit button + ongoing work on fetch requests

--- app/src/components/ProfilePage.jsx
+++ app/src/components/ProfilePage.jsx
@@ -1,7 +1,36 @@
+import { useEffect } from "react"
 import { useState } from "react"
 
 
 function ProfilePage() {
+
+    // const [firstName, setFirstName] = useState("");
+    // const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    // const [country, setCountry] = useState("");
+    // const [city, setCity] = useState("");
+
+    async function fetchUserInfo(){
+        let rawResponse = await fetch('/data/users/1')
+        let response = await rawResponse.json();
+        console.log(response);
+    }
+    
+    
+    fetchUserInfo();
+
+
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //     const requestOptions = {
+    //       method: 'POST',
+    //       headers: { 'Content-type': 'application/json' },
+    //       body: JSON.stringify({ email}),
+    //     };
+    //     let response = await fetch('/data/users', requestOptions);
+    //     response = await response.json();
+    //   };
+    
     return <>
         <h1 id="profile-title">Your Profile</h1>
         <form action="" className="profile-form">
@@ -26,6 +55,7 @@ function ProfilePage() {
                 <input className="form-input" type="text" name="city" id="city" />
             </div>
             <div className="form-field" id="update">
+                <button className="form-input">Edit info</button>
                 <input className="form-input" type="submit" value={"Update your personal info"} />
             </div>
             <button id="sign-out">Sign out</button>
