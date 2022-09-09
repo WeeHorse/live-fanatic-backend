89fb1f28d3b93d0e056795166fed2fde6b569500
anneliey <annelieyan@gmail.com>
2022-09-09 11:35:27 +0200

    NJ-68 Add event page

---
+++ app/src/components/EventPage.jsx
@@ -0,0 +1,20 @@
+import Card from './Card';
+
+function EventPage() {
+  const data = {
+    title: 'Baby',
+    venue: 'Plan B',
+    date: '2022-10-09',
+    artist: 'Justin Bieber',
+    image:
+      'https://th.bing.com/th/id/OIP.wfg6sIM0YAd9wYn9Vj8QtwHaJf?pid=ImgDet&rs=1',
+  };
+  return (
+    <>
+      <div className='container d-lg-flex'>
+        <Card data={data} />
+      </div>
+    </>
+  );
+}
+export default EventPage;
