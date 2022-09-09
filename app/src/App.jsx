aaf4def6801b65d8066b9ae4b37a69aa5750c448
anneliey <annelieyan@gmail.com>
2022-09-09 11:37:26 +0200

    NJ-68 Add imports

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Card from './components/Card';
+import EventPage from './components/EventPage';
 
 function App() {
-    return <>
-        <h1>App</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere in dolore neque, odit consequatur sit explicabo, nisi perspiciatis mollitia, obcaecati at labore doloremque nihil error et blanditiis. Consequuntur deserunt deleniti reiciendis eaque tempore odit, repudiandae quas a accusantium, ad ab dignissimos ullam. Doloremque at voluptas excepturi esse repellendus quidem ipsum.</p>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/event' element={<EventPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
+  );
 }
 
-export default App
+export default App;
