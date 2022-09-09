5ce494b1e0f989bd60109c16d588fa9e2db2a86e
AntonTapper <ant.tapper@gmail.com>
2022-09-09 13:47:34 +0200

    NJ-56 added icons TK

--- app/src/components/Hamburger.jsx
+++ app/src/components/Hamburger.jsx
@@ -2,46 +2,27 @@ function Hamburger({ isOpen }) {
 
     return (
         <>
-            <div className="hamburger">
-                <div className="burger burger1"></div>
-                <div className="burger burger2"></div>
-                <div className="burger burger3"></div>
+            <div className="hamburger" >
+                <span class="material-symbols-outlined" id="burger">
+                    menu
+                </span>
+                <span class="material-symbols-outlined" id="cross">
+                    close
+                </span>
             </div>
 
-            <style jsx>{`
-                .hamburger{
-                    width:2rem;
-                    height:2rem;
-                    display:flex;
-                    justify-content:space-around;
-                    flex-flow:column nowrap;
-                    z-index:10;
-                    overflow: hidden;
-                }
+            <style jsx="true">{`
 
-                .burger{
-                    width:2rem;
-                    height:0.25rem;
-                    border-radius:10px;
-                    background-color:black;
-                    transform-origin:1px;
-                    transition:all 0.3s linear;
+                
+                #burger{
+                    display: ${isOpen ? 'none' : 'grid'};
                 }
-
-                .burger1{
-                    transform: ${isOpen ? 'rotate(45deg)' : 'rotate(0)'};
+                #cross{
+                    display: ${isOpen ? 'grid' : 'none'};
                 }
-                .burger2{
-                    transform: ${isOpen ? 'translateX(100%)' : 'translateX(0)'};
-                    opacity: ${isOpen ? 0 : 1};
-                }
-                .burger3{
-                    transform: ${isOpen ? 'rotate(-45deg)' : 'rotate(0)'};
-                }
-
                 @media (min-width: 767px){
 
-                    .burger{
+                    .hamburger{
                         display:none;
                     }
                     
