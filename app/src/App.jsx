6b6c2b0b57341d8db054333a9e17f0ef9b78321e
krubear <torkild.kruber@gmail.com>
2022-09-09 12:55:00 +0200

    NJ-56 Premerge

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './components/HomePage';
 import Logout from './components/Logout';
+import Nav from './components/Nav'
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Homepage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <Nav />
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Homepage />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
