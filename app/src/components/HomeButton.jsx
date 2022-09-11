13613be03353b3db7ee5702bdd41b64d7f1815a5
Dannebanne94 <92047500+Dannebanne94@users.noreply.github.com>
2022-09-11 23:57:51 +0200

    NJ-84: Added functionality and css.

--- app/src/components/HomeButton.jsx
+++ app/src/components/HomeButton.jsx
@@ -1,11 +1,11 @@
-
+import {Link} from 'react-router-dom';
 
 export default function Homebutton() {
     
 
     return <>
         <div id="home">
-            <img id="home-button" src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/iyumhngjnhqawshrvfrf" alt="logo" />
+            <Link to="/" id="home-link"> <img id="home-button" src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/iyumhngjnhqawshrvfrf" alt="logo" /></Link>
         </div>
 
     </>
