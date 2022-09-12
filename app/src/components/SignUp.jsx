b2c237a2a979c00656d21f618f38a5659531112d
Annelie Yan <annelieyan@gmail.com>
2022-09-12 11:40:00 +0200

    NJ-44 Add Sign up page and component

---
+++ app/src/components/SignUp.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [verifyPassword, setVerifyPassword] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  // const handlePasswordValidation = () => {
+  //   if (password !== verifyPassword) {
+  //     setConfirmPasswordError("Password do not match");
+  //     return confirmPasswordError;
+  //   }
+  // };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // handlePasswordValidation();
+    // if (!confirmPasswordError) {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    };
+    let response = await fetch("/data/users", requestOptions);
+    if (response.ok) {
+      // response = await response.json();
+      console.log(response.headers);
+      // navigate("/");
+    } else {
+      setError(response.statusText);
+    }
+    // }
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        {error && <span className="login-form__login-error">{error}</span>}
+        <label>Email</label>
+        <input
+          className="custom-input"
+          type="text"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Password</label>
+        <input
+          className="custom-input"
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {/* <label>Verify password</label>
+        <input
+          className="custom-input"
+          type="password"
+          required
+          value={verifyPassword}
+          onChange={(e) => setVerifyPassword(e.target.value)}
+        /> */}
+        <button className="custom-red-btn">Sign up</button>
+        {/* {confirmPasswordError && <span>{confirmPasswordError}</span>} */}
+      </form>
+    </div>
+  );
+}
+export default SignUp;
