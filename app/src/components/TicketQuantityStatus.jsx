9f989ae4352756cef5943bca5c728c8b139be1e1
Annelie Yan <annelieyan@gmail.com>
2022-09-12 10:48:41 +0200

    NJ-92 Update TicketQuantityStatus.jsx

--- app/src/components/TicketQuantityStatus.jsx
+++ app/src/components/TicketQuantityStatus.jsx
@@ -1,29 +1,13 @@
+import RedStatus from "../assets/RedStatus.svg";
+import GreenStatus from "../assets/GreenStatus.svg";
 function TicketQuantityStatus({ quantity }) {
   return (
     <div className="quantity-status">
       {quantity == 0 && <span>sold out</span>}
       {quantity < 20 && quantity > 0 && (
-        <svg
-          width="13"
-          height="13"
-          viewBox="0 0 13 13"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="6.5" cy="6.5" r="6.5" fill="#EB0000" />
-        </svg>
+        <img src={RedStatus} alt="greenstatus" />
       )}
-      {quantity > 20 && (
-        <svg
-          width="13"
-          height="13"
-          viewBox="0 0 13 13"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="6.5" cy="6.5" r="6.5" fill="#00DD31" />
-        </svg>
-      )}
+      {quantity > 20 && <img src={GreenStatus} alt="redstatus" />}
     </div>
   );
 }
