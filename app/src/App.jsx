596b0defc3c3a73e96f2202e420a3464532a2244
AntonTapper <ant.tapper@gmail.com>
2022-09-12 10:36:55 +0200

    NJ-41 updated imports

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,11 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './components/HomePage';
 // import ProfilePage from './components/profilePage';
 import ProfilePage from './components/ProfilePage';
+import Nav from './components/Nav'
 
 function App() {
   return (
     <>
+    <Nav/>
       <BrowserRouter>
+        
         <Routes>
           <Route path='/' element={<Homepage />} />
           <Route path='/profile' element={<ProfilePage />} />
