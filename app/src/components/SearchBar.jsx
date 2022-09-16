a614290b080cc135ce2dd34dc24cce9fb0f5c5e9
krubear <torkild.kruber@gmail.com>
2022-09-16 17:00:57 +0200

    NJ-128 added crude venue/artist search function

--- app/src/components/SearchBar.jsx
+++ app/src/components/SearchBar.jsx
@@ -4,6 +4,7 @@ import closeSvg from '../assets/close.svg'
 import searchSvg from '../assets/search.svg'
 import useFetch from "../hooks/useFetch";
 import { Link } from 'react-router-dom';
+import ArtistEvents from "./ArtistEvents";
 
 
 
@@ -11,14 +12,26 @@ function SearchBar({ placeholder }) {
     const [filteredData, setFilteredData] = useState([]);
     const [wordEntered, setWordEntered] = useState("");
 
-    const { error, isPending, data: artists } = useFetch("/data/concert_details");
+    const { error, isPending, data: stuff } = useFetch("/data/concert_details");
+
+    const [oneOrTwo, setOneOrTwo] = useState(false);
+    const toggleOneOrTwo = () => {
+        setOneOrTwo(!oneOrTwo);
+    };
+
 
 
     const handleFilter = (event) => {
         const searchWord = event.target.value;
         setWordEntered(searchWord);
-        const newFilter = artists.filter((artist) => {
-            return artist.name.toLowerCase().includes(searchWord.toLowerCase());
+        const newFilter = stuff.filter((artist) => {
+            if (artist.artist_name.toLowerCase().includes(searchWord.toLowerCase())) {
+                setOneOrTwo(false);
+                return artist.artist_name.toLowerCase().includes(searchWord.toLowerCase())
+            } else if (artist.venue_name.toLowerCase().includes(searchWord.toLowerCase())) {
+                setOneOrTwo(true);
+                return artist.venue_name.toLowerCase().includes(searchWord.toLowerCase())
+            }
         });
 
         if (searchWord === "") {
@@ -52,11 +65,19 @@ function SearchBar({ placeholder }) {
                 </div>
             </div>
             {filteredData.length != 0 && (
-                <div className="dataResult" key={artists.id}>
+                <div className="dataResult" key={stuff.artist_id}>
                     {filteredData.slice(0, 15).map((artist, key) => {
                         return (
                             <div className="dataItem">
-                                <Link to={`/artist/${artist.id}`}>{artist.name}</Link>
+                                <Link to={`/artist/${artist.artist_id}`}>
+                                    {oneOrTwo == false && (
+                                        artist.artist_name
+                                    )
+                                    }
+                                    {oneOrTwo == true && (
+                                        artist.venue_name
+                                    )}
+                                </Link>
                             </div>
                         );
                     })}
