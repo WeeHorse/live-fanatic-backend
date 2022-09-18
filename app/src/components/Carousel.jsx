cc9f3930a78de3cec8294d95edcfee64aa8ac84a
ManneWas <mannewas@protonmail.com>
2022-09-18 09:33:23 +0200

    NJ carousel stuff

--- app/src/components/Carousel.jsx
+++ app/src/components/Carousel.jsx
@@ -1,31 +1,55 @@
 import CarouselCard from "./CarouselCard";
 import {useEffect, useState} from "react";
+import leftArrow from "../assets/chevron_left.svg";
+import rightArrow from "../assets/chevron_right.svg";
 
 export default function Carousel(props) {
+
+    const items = props['items'], max = items.length - 1
+
     const [index, setIndex] = useState(0)
-    const items = props['items']
-    const max = items.length - 1
+
     const [item, setItem] = useState(items[0])
-    const incrementIndex = () => {
-        console.log('current index:'+index)
-        if (index >= max) setIndex(0)
-        else setIndex(index + 1)
+
+
+    function incrementIndex() {
+        if (index >= max) {
+            setIndex(0)
+            return 0
+        } else {
+            setIndex(index + 1)
+            return index + 1
+        }
     }
+
     const decrementIndex = () => {
-        console.log('current index:'+index)
         if (index === 0) setIndex(max)
         else setIndex(index - 1)
     }
+
     useEffect(() => {
         setItem(items[index])
-
-        console.log('new index:'+index)
+        console.log(index)
     }, [index])
-    if (!item) return <></>
-    return <>
-        <CarouselCard image={item["image"]} type={props['type']} id={item.id}/>
-        <button onClick={incrementIndex}>Next Item!</button>
-        <button onClick={decrementIndex}>Previous Item!</button>
-    </>
 
+    if (!item) return <></>
+
+    return <div className={'carousel'}>
+        <a className={'scroll-button'} onClick={decrementIndex} href={'#slide-' + index}>
+            <img src={leftArrow} alt="<-"/>
+        </a>
+        {items.map((item, itemIndex) => {
+            return <a className={'index'} onClick={() => {
+                setIndex(itemIndex)
+            }} href={'#slide-' + itemIndex}>{itemIndex}</a>
+        })}
+        <div className="slides">
+            {items.map((item, itemIndex) =>
+                <CarouselCard image={item["image"]} type={props['type']} id={itemIndex} itemId={item.id}/>
+            )}
+        </div>
+        <a className={'scroll-button'} onClick={incrementIndex} href={'#slide-' + index}>
+            <img src={rightArrow} alt=""/>
+        </a>
+    </div>
 }
--- app/src/components/CarouselCard.jsx
+++ app/src/components/CarouselCard.jsx
@@ -1,14 +1,16 @@
 import {Link} from "react-router-dom";
 
 export default function CarouselCard(params) {
-    const image = params['image']
-    const type = params['type']
-    const id = params['id']
-    return <div className={'carousel-card'}>
-        <Link to={type+id} id={id}>
+    const image = params.image
+    const type = params.type
+    const id = params.itemId
+    return <div className={'carousel-card'} id={'slide-'+params.id}>
+        <Link to={type + id} id={id}>
             <img src={image} alt=" image"/>
         </Link>
-        <Link to={type}><button>Click me!</button></Link>
+        <Link to={type}>
+            <button>Click me!</button>
+        </Link>
     </div>
 
 }
