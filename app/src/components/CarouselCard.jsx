4cf0f493da357c212de78216867ecabd7aecc080
ManneWas <mannewas@protonmail.com>
2022-09-21 09:50:59 +0200

    a

--- app/src/components/CarouselCard.jsx
+++ app/src/components/CarouselCard.jsx
@@ -4,7 +4,7 @@ export default function CarouselCard(props) {
     const image = props.image
     const type = props.type
     const id = props.itemId
-    return <div className={'carousel-card'} id={`slide-${props['key']}-${props['id']}`}>
+    return <div className={'carousel-card'} id={`slide-${props['uid']}-${props['id']}`}>
         <Link to={type + id} id={id}>
             <img src={image} alt=" image"/>
         </Link>
